788d9976c91fcabe8dcc611580295d59f15ebcd4
Silvana Bendahan Gloria <sbendahan81@gmail.com>
2022-08-11 10:23:57 -0300

    Fin desafio complementario

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 //Sorteo de la lotería. 3 intentos. Números ganadores: 15 y 80
 
-/*let numero = "";
+let numero1 = "";
 
 for (i=0; i<3; i++) {
-    numero = parseInt (prompt ("Ingresá un número"));
-    if (numero == 15) {
+    numero1 = parseInt (prompt ("Ingresá un número"));
+    if (numero1 == 15) {
         alert ("¡Ganaste un auto 0km!");
         break;
     } 
 
-    else if (numero == 80) {
+    else if (numero1 == 80) {
         alert ("¡Afortunado! Ganaste una entrada al cine");
         break;
     }
@@ -17,30 +17,32 @@ for (i=0; i<3; i++) {
     else {
         alert ("ups, seguí participando");
     }
-}*/
+}
 
 
 
 
-//Sorteo con ganador (casi) asegurado
+//Sorteo con ganador (casi) asegurado (Exit con 0 (cero))
 
-/*let numero = prompt ("Escriba un número");
+let numero2 = prompt ("Escriba un número");
 
-while (numero != 0) {
+while (numero2 != 0) {
         
     alert ("¡Ganaste un premio!");
-    numero = prompt ("Escribí otro número");
+    numero2 = prompt ("Escribí otro número");
 
-}*/
+}
+
+
 
 //Opción con Do... While
-let numero = prompt ("Escriba un número");
+let numero3 = prompt ("Escriba un número");
 
 do {
     alert ("¡Ganaste un premio!");
-    numero = prompt ("Escribí otro número");
+    numero3 = prompt ("Escribí otro número");
 }
-while (numero != 0)
+while (numero3 != 0)
         
   
 
